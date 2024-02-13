4c0574a4940e1dd4acbb94aca5d36e13279011b8
Rinat <atkbrc354@mail.ru>
2024-02-13 11:59:17 +0300

    add: fc-daygrid-day-number

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     calendar.render();
 
     // Создание списка доступных таймслотов
-    const timeslots = ['10:00', '10:30', '11:00', '11:30', '12:00', '12:30'];
+    const timeslots = ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00'];
     const timeslotList = document.getElementById('timeslotList');
     
     timeslots.forEach(time => {
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         // Добавление реакции на клик по элементу списка
         timeslotItem.addEventListener('click', function() {
-            alert('Выбранное время: ' + time);
+            
         });
     });
 });
